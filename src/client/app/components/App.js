ca5ffa1bef5789d7ef16b1cdf82712870a15aeb2
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-07-15 14:23:04 +0900

    feat(DailySwitch): extract from App

--- src/client/app/components/App.js
+++ src/client/app/components/App.js
@@ -1,150 +1,57 @@
 /* @flow */
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { Switch, withRouter } from 'react-router-dom';
 import styled from 'styled-components';
-import { createRefetchContainer, graphql } from 'react-relay';
+import { createFragmentContainer, graphql } from 'react-relay';
 import PropsRoute from '../../shared/components/PropsRoute';
-import { startOfDay } from '../../../server/shared/utils/DateUtils';
-import type { App_viewer } from './__generated__/App_viewer.graphql';
-import DailyReportPage from './DailyReportPage';
+import DailySwitch from './DailySwitch';
 import DashboardPage from './DashboardPage';
+import NavBar from './NavBar';
 import ProjectsPage from './ProjectsPage';
-import DailySchedulePage from './DailySchedulePage';
 import TaskSetsPage from './TaskSetsPage';
 
 const Wrapper = styled.div`padding: 1.5rem;`;
 
-type Props = { viewer: App_viewer };
-
-type State = { currentDate: Date };
-
-// TODO: Extract into other file
-function previousDay(date) {
-  return startOfDay(new Date(date.getTime() - 24 * 60 * 60 * 1000));
-}
-
-// TODO: Extract into other file
-function nextDay(date) {
-  return startOfDay(new Date(date.getTime() + 24 * 60 * 60 * 1000));
-}
-
 export class App extends React.Component {
-  props: Props;
-  state: State;
-
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      currentDate: startOfDay(new Date()),
-    };
-  }
-
-  _handleGoToPreviousDayClick = () => {
-    this._goToPreviousDay();
-  };
-
-  _handleGoToNextDayClick = () => {
-    this._goToNextDay();
-  };
-
-  _goToPreviousDay() {
-    const { relay } = this.props;
-
-    this.setState(
-      ({ currentDate }) => ({ currentDate: previousDay(currentDate) }),
-      () => {
-        const { currentDate } = this.state;
-        const refetchVariables = () => ({ date: currentDate });
-
-        relay.refetch(refetchVariables, null);
-      },
-    );
-  }
-
-  _goToNextDay() {
-    const { relay } = this.props;
-
-    this.setState(
-      ({ currentDate }) => ({ currentDate: nextDay(currentDate) }),
-      () => {
-        const { currentDate } = this.state;
-        const refetchVariables = () => ({ date: currentDate });
-
-        relay.refetch(refetchVariables, null);
-      },
-    );
-  }
-
   render() {
     const { viewer } = this.props;
-    const { currentDate } = this.state;
 
     return (
       <Wrapper>
-        <ul>
-          <li>
-            <Link to="/">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/dailySchedule">DailySchedule</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-          <li>
-            <Link to="/taskSets">TaskSets</Link>
-          </li>
-          <li>
-            <Link to="/dailyReport">DailyReport</Link>
-          </li>
-          <li>
-            <Link to="/options">Options</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/dailyReportTemplate">DailyReportTemplate</Link>
-          </li>
-        </ul>
-        <button onClick={this._handleGoToPreviousDayClick}>Previous Day</button>
-        <button onClick={this._handleGoToNextDayClick}>Next Day</button>
-        <PropsRoute exact path="/" component={DashboardPage} viewer={viewer} />
-        <PropsRoute
-          path="/dailySchedule"
-          component={DailySchedulePage}
-          defaultDate={currentDate}
-          viewer={viewer}
-        />
-        <PropsRoute path="/projects" component={ProjectsPage} viewer={viewer} />
-        <PropsRoute path="/taskSets" component={TaskSetsPage} viewer={viewer} />
-        <PropsRoute
-          path="/dailyReport"
-          component={DailyReportPage}
-          viewer={viewer}
-        />
+        <NavBar />
+        <Switch>
+          <PropsRoute
+            exact
+            path="/"
+            component={DashboardPage}
+            viewer={viewer}
+          />
+          <PropsRoute
+            path="/projects"
+            component={ProjectsPage}
+            viewer={viewer}
+          />
+          <PropsRoute
+            path="/taskSets"
+            component={TaskSetsPage}
+            viewer={viewer}
+          />
+          <PropsRoute path="/daily" component={DailySwitch} viewer={viewer} />
+        </Switch>
       </Wrapper>
     );
   }
 }
 
 export default withRouter(
-  createRefetchContainer(
+  createFragmentContainer(
     App,
     graphql.experimental`
-      fragment App_viewer on User @argumentDefinitions(date: { type: "Date" }) {
+      fragment App_viewer on User {
         id
-        ...DailyReportPage_viewer @arguments(date: $date)
+        ...DailySwitch_viewer
         ...ProjectsPage_viewer
-        ...DailySchedulePage_viewer @arguments(date: $date)
         ...TaskSetsPage_viewer
-      }
-    `,
-    graphql.experimental`
-      query AppRefetchQuery($date: Date) {
-        viewer {
-          ...App_viewer @arguments(date: $date)
-        }
       }
     `,
   ),
---
+++ src/client/app/components/DailySwitch.js
@@ -0,0 +1,116 @@
+/* @flow */
+import React from 'react';
+import { createRefetchContainer, graphql } from 'react-relay';
+import { Switch, withRouter } from 'react-router-dom';
+import { startOfDay } from '../../../server/shared/utils/DateUtils';
+import PropsRoute from '../../shared/components/PropsRoute';
+import DailySchedulePage from './DailySchedulePage';
+import DailyReportPage from './DailyReportPage';
+
+// TODO: Extract into other file
+function previousDay(date) {
+  return startOfDay(new Date(date.getTime() - 24 * 60 * 60 * 1000));
+}
+
+// TODO: Extract into other file
+function nextDay(date) {
+  return startOfDay(new Date(date.getTime() + 24 * 60 * 60 * 1000));
+}
+
+type Props = { viewer: any };
+
+type State = { currentDate: Date };
+
+export class DailySwitch extends React.Component {
+  props: Props;
+  state: State;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      currentDate: startOfDay(new Date()),
+    };
+  }
+
+  _handleGoToPreviousDayClick = () => {
+    this._goToPreviousDay();
+  };
+
+  _handleGoToNextDayClick = () => {
+    this._goToNextDay();
+  };
+
+  _goToPreviousDay() {
+    const { relay } = this.props;
+
+    this.setState(
+      ({ currentDate }) => ({ currentDate: previousDay(currentDate) }),
+      () => {
+        const { currentDate } = this.state;
+        const refetchVariables = () => ({ date: currentDate });
+
+        relay.refetch(refetchVariables, null);
+      },
+    );
+  }
+
+  _goToNextDay() {
+    const { relay } = this.props;
+
+    this.setState(
+      ({ currentDate }) => ({ currentDate: nextDay(currentDate) }),
+      () => {
+        const { currentDate } = this.state;
+        const refetchVariables = () => ({ date: currentDate });
+
+        relay.refetch(refetchVariables, null);
+      },
+    );
+  }
+
+  render() {
+    const { viewer } = this.props;
+    const { currentDate } = this.state;
+
+    return (
+      <div>
+        <button onClick={this._handleGoToPreviousDayClick}>Previous Day</button>
+        <button onClick={this._handleGoToNextDayClick}>Next Day</button>
+        <Switch>
+          <PropsRoute
+            path="/daily/schedule"
+            component={DailySchedulePage}
+            defaultDate={currentDate}
+            viewer={viewer}
+          />
+          <PropsRoute
+            path="/daily/report"
+            component={DailyReportPage}
+            viewer={viewer}
+          />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default withRouter(
+  createRefetchContainer(
+    DailySwitch,
+    graphql.experimental`
+      fragment DailySwitch_viewer on User
+        @argumentDefinitions(date: { type: "Date" }) {
+        id
+        ...DailyReportPage_viewer @arguments(date: $date)
+        ...DailySchedulePage_viewer @arguments(date: $date)
+      }
+    `,
+    graphql.experimental`
+      query DailySwitchRefetchQuery($date: Date) {
+        viewer {
+          ...DailySwitch_viewer @arguments(date: $date)
+        }
+      }
+    `,
+  ),
+);
