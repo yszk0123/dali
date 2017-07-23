690e86e01fc9d884fb6de55e6d841853493f6180
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-07-23 13:26:10 +0900

    fix(client): fix touch detection

--- src/client/shared/utils/isTouchSupported.js
+++ src/client/shared/utils/isTouchSupported.js
@@ -1,5 +1,5 @@
 /* @flow */
 
 export default function isTouchSupported(): boolean {
-  return window.DocumentTouch && document instanceof window.DocumentTouch;
+  return 'ontouchstart' in window || navigator.msMaxTouchPoints;
 }
