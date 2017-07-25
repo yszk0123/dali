ef77157cd145f65e93bc36089e6fa2d2cfbc12ca
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-07-26 01:56:22 +0900

    refactor(TaskSet): extract DoneCheckbox

--- src/client/app/containers/TaskSetItem.js
+++ src/client/app/containers/TaskSetItem.js
@@ -9,6 +9,7 @@ import openLinkProjectModal from '../../redux/actions/openLinkProjectModal';
 import Icon from '../components/Icon';
 import TitlePlaceholder from '../components/TitlePlaceholder';
 import TitleInput from '../components/TitleInput';
+import DoneCheckbox from '../components/DoneCheckbox';
 
 type Props = {
   taskSet: any,
@@ -70,11 +71,7 @@ export class TaskSetItem extends React.Component {
 
     return (
       <Wrapper>
-        <input
-          type="checkbox"
-          checked={taskSet.done}
-          onChange={this._handleDoneChange}
-        />
+        <DoneCheckbox done={taskSet.done} onChange={this._handleDoneChange} />
         <TitleInput
           title={taskSet.title}
           onChange={this._handleTaskSetTitleChange}
