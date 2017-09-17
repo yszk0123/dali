16d90e528d79faf21c5da2f0e1f7f33318980e60
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-09-17 16:32:54 +0900

    refactor(client): remove unused old/

--- old/client/app/containers/__tests__/ProfilePage.test.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { shallow } from 'enzyme';
-import ProfilePage from '../ProfilePage';
-
-test('renders', () => {
-  const wrapper = shallow(<ProfilePage />);
-
-  expect(wrapper).toBeDefined();
-});
